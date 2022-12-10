fe187792285bbc1cecf4cdc84715cffc05680700
frulcino <ghjuber@gmail.com>
2022-12-10 19:01:28 +0100

    error found

--- Analyse_de_donnees/group17.R
+++ Analyse_de_donnees/group17.R
@@ -12,22 +12,20 @@ library(ggplot2)
 
 #1b
 disj_tab <- function(X){
-  #doesn't handle NA values, i'm not sure how afcp handles them
   U <- data.frame()
   for (variable in names(X)){
     for (i in 1:nrow(X)) {
       value <- X[i,variable]
-      name <- as.character(value) #paste(as.character(variable),as.character(value), sep = "_") if all the questions have same answers
+      name <- as.character(value) 
       U[i,name] = 1
     }
   }
-  #substitues NA values with 0
   U[is.na(U)] = 0
   return(U)
 }
 
 
-Qprod <- function(u, v,Q){ #this is a scalar product
+Qprod <- function(u, v,Q){
   return(t(u)%*% Q %*% v)
 }
 
@@ -35,15 +33,8 @@ Qnorm <- function(u,Q){
   return(sqrt(Qprod(u,u,Q)))
 }
 
-#calculates the coordinates of the v projection on the span(cols of A)
-#in respect to the columns of A
-A_coords <- function(A,v) {
-  
-}
 
-
-
-#Exercise 1 (Putting all together)
+#Exercise 1 
 AFCM <- function(X) {
   n0 <- nrow(X)
   p <- ncol(X)
@@ -69,25 +60,18 @@ AFCM <- function(X) {
   V <- t(X) %*% D %*% X
   Q_sqrt <- sqrt(Q)
   VQ <- V %*% Q
-  ev <- eigen(Q_sqrt %*% V %*% Q_sqrt) #eigenvalues accessed by $values vectors by $vectors
+  ev <- eigen(Q_sqrt %*% V %*% Q_sqrt) 
   ord <- order(Re(ev$values), decreasing = TRUE)
-  ev$vectors <- inv(Q_sqrt) %*% ev$vectors[,ord] #obtaines eigenvectors for VQ (look fischier pdf "answer" added)
+  ev$vectors <- inv(Q_sqrt) %*% ev$vectors[,ord] #obtaines eigenvectors for VQ 
   ev$values <- ev$values[ord]
   
-  
-  #Small eigen values (which should be 0 but approximation errors end up imaginary and then stuff gets messed up)
   A <- c()
    for (i in 1:length(ev$values)){
-    #(sprintf("The ith vector is %s with Qnorm %f",paste(A[,i], collapse = " "),Qnorm(A[,i],Q)))
-    if (Re(ev$values[i]) > 0){ #We are only interested in the axis with positive eigenvalues (inertia)
+    if (Re(ev$values[i]) > 0){ 
       A <- cbind(A,ev$vectors[,i]/Qnorm(ev$vectors[,i], Q))
     }
-    
-    
-    #print(sprintf("The ith vector is %s with Qnorm %f",paste(A[,i], collapse = " "),Qnorm(A[,i],Q)))
   }
-    print(A)
-    
+
   #1.e
   C = c()
   for (i in 1:ncol(A)){  
@@ -101,19 +85,17 @@ AFCM <- function(X) {
   A_tilde <- A #ith line = coordinates of ith-variables on c(k) basis
   C_tilde <- C #ith line =coordinates of ith-individue on a(k) basis
   for (i in 1:ncol(A)) {
-    A_tilde[,i] <- A_tilde[,i] * ev$values[i]
-    C_tilde[,i] <- C_tilde[,i] * ev$values[i]
+    A_tilde[,i] <- A_tilde[,i] * sqrt(ev$values[i])
+    C_tilde[,i] <- C_tilde[,i] * sqrt(ev$values[i])
   }
   
   #1.h Renvoyez la liste résultat comportant les pourcentages d’inertie Λ et les matrices A, C, A˜ et C˜.
   A_tilde <- as.data.frame(A_tilde, row.names = names(disjoint_table))
   C_tilde <- as.data.frame(C_tilde, row.names = ind_names)
-  col_names <- c()
-  
-  
   return(list(percentages_intertie, A, C, A_tilde, C_tilde))
 }
 
+#Exercise 2
 plot_individues <- function(afcm) {
   C_tilde <- afcm[[5]]
   ggplot(C_tilde, aes(x = C_tilde[,1], y = C_tilde[,2], label = row.names(C_tilde))) +
@@ -121,33 +103,21 @@ plot_individues <- function(afcm) {
     geom_text(position=position_jitter(width=1,height=1) )
 }
 
-
+#Exercise 3
 plot_variables <-  function(afcm) {
   A_tilde <- afcm[[4]]
   ggplot(A_tilde, aes(x = A_tilde[,1], y = A_tilde[,2], label = row.names(A_tilde))) +
     geom_point() +
     geom_text(position=position_jitter(width=1,height=1) )
-    
-  
 }
 
-#Test
+#Exercise 4
 exacm <- read.csv("exacm.csv", header = TRUE, row.names = 1, sep = ";")
 
 AFCM_exacm <- AFCM(exacm)
 
-#Selection des axes
+#selection axis
 barplot(unlist(AFCM_exacm[1]))
-C_tilde <- as.data.frame(AFCM_exacm[5])
-A_tilde <- as.data.frame(AFCM_exacm[4])
-
-plot(C_tilde[,1], C_tilde[,2])
-text(C_tilde[,1]+1, C_tilde[,2], labels = row.names(C_tilde))
-
-
-
-#plot(A_tilde, axes = c(1,2))
-#text(A_tilde[,1]+1, A_tilde[,2], labels = row.names(A_tilde))
 
 
 plot_individues(AFCM_exacm)
